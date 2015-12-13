6a1b4060c51163ad9ec43b068f4eb7f70b0e91d3
Cory Johns <johnsca@gmail.com>
2015-12-13 01:25:48 -0500

    Removed unnecessary layer option

--- lib/charms/hadoop.py
+++ lib/charms/hadoop.py
@@ -1,10 +1,10 @@
-from charms.layer import LayerOptions
 from jujubigdata.utils import DistConfig
 from jujubigdata.handlers import HadoopBase
 
 
 def get_hadoop_base():
-    layer_opts = LayerOptions('hadoop-base')
+    dist_keys = ['vendor', 'hadoop_version', 'packages',
+                 'groups', 'users', 'dirs', 'ports']
     dist_config = DistConfig(filename='dist.yaml',
-                             required_keys=layer_opts['dist_keys'])
+                             required_keys=dist_keys)
     return HadoopBase(dist_config)
