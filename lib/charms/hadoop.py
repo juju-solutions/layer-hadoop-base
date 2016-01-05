354cdbf2c81457d7db4e8638dfbc9a073fc46cf7
Cory Johns <johnsca@gmail.com>
2016-01-04 20:27:02 -0500

    Added helper for DistConfig

--- lib/charms/hadoop.py
+++ lib/charms/hadoop.py
@@ -2,9 +2,12 @@ from jujubigdata.utils import DistConfig
 from jujubigdata.handlers import HadoopBase
 
 
+def get_dist_config(required_keys):
+    return DistConfig(filename='dist.yaml',
+                      required_keys=required_keys)
+
+
 def get_hadoop_base():
     dist_keys = ['vendor', 'hadoop_version', 'packages',
                  'groups', 'users', 'dirs', 'ports']
-    dist_config = DistConfig(filename='dist.yaml',
-                             required_keys=dist_keys)
-    return HadoopBase(dist_config)
+    return HadoopBase(get_dist_config(dist_keys))
