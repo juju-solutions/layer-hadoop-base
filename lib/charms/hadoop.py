52f057e2c195b595dd44e10780b76bccf722d580
Cory Johns <johnsca@gmail.com>
2016-01-05 19:28:43 -0500

    Enable dist.yaml overrides in layer.yaml
    This allows us to drop dist.yaml from charms using hadoop-base and only
    require a layer.yaml file.  (At some point, we might want to fold
    resources.yaml into layer.yaml as well.)
    
    This also allows us to side-step the list merging issue with charm build
    (https://github.com/juju/charm-tools/issues/85) for the packages and
    groups options.

--- lib/charms/hadoop.py
+++ lib/charms/hadoop.py
@@ -1,10 +1,22 @@
 from jujubigdata.utils import DistConfig
 from jujubigdata.handlers import HadoopBase
+from charms.layer import LayerOptions
 
 
 def get_dist_config(required_keys):
-    return DistConfig(filename='dist.yaml',
+    dist = DistConfig(filename='dist.yaml',
                       required_keys=required_keys)
+    opts = LayerOptions('hadoop-base')
+    for key in ('vendor', 'hadoop_version'):
+        if key in opts:
+            dist.dist_config[key] = opts[key]
+    for key in ('packages', 'groups'):
+        if key in opts:
+            dist.dist_config[key] = list(set(dist.dist_config[key]) | set(opts[key]))
+    for key in ('users', 'dirs', 'ports'):
+        if key in opts:
+            dist.dist_config[key].update(opts[key])
+    return dist
 
 
 def get_hadoop_base():
